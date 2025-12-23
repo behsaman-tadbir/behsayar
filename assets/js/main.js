0e9b53dee338570b26608b0d2ea3c69edbcc1e59
behsaman-tadbir <dadashi.behsaman@gmail.com>
2025-12-23 11:52:47 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,3 @@ function toggleMenu() {
     .getElementById('mobileMenu')
     .classList.toggle('show');
 }
-function toggleMenu() {
-  document
-    .getElementById('mobileMenu')
-    .classList.toggle('show');
-}
